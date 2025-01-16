a1c4c33f0f9b56d25d8944c199b8b302382fa75d
Huzaifa Iqbal <huzaifaiqbal89@gmail.com>
2025-01-16 19:14:50 +0500

    changs updated

--- components/innerpage/ContentSection/ContentSection.tsx
+++ components/innerpage/ContentSection/ContentSection.tsx
@@ -1,45 +1,86 @@
 import Image from "next/image";
-import React from "react";
+import React, { memo } from "react";
 import s from "./ContentSection.module.scss";
 
 interface SubHeadingProps {
-  logo: string;
-  description: string[];
-  image: string;
+  logo?: string;
+  description?: string[];
+  image?: string;
+  type?: string;
+  contentType?: "";
+  minDescription?: boolean;
+  leftImg?: boolean;
 }
 
 const ContentSection: React.FC<SubHeadingProps> = ({
   logo,
   description,
   image,
+  type,
+  minDescription,
+  leftImg,
 }) => {
   return (
-    <div className={s.section}>
-      <div className={s.content}>
+    <div className={`${s.section} ${minDescription ? s.leftImgSection : ""}`}>
+      {leftImg && image && (
+        <figure className={s.image}>
+          <Image src={image} alt="Content Image" width={800} height={800} />
+        </figure>
+      )}
+
+      <div className={`${s.content} ${leftImg ? s.leftImgContent : ""}`}>
         {/* Logo Section */}
-        <div className={s.logo}>
-          <Image src={logo || ""} alt="Logo" width={400} height={108} />
-        </div>
+        {logo && (
+          <div className={s.logo}>
+            <Image src={logo} alt="Logo" width={400} height={108} />
+          </div>
+        )}
         {/* First Description */}
-        <p className={s.description}>{description[0]}</p>
-      </div>
-
-      {/* Image Section */}
-      <figure className={s.image}>
-        <Image src={image || ""} alt="Content Image" width={800} height={800} />
-      </figure>
-
-      {/* Additional Descriptions */}
-      <div className={s.addContent}>
-        {description &&
-          description.slice(1).map((text, index) => (
-            <p className={s.description} key={index}>
+        {!minDescription && description?.[0] && (
+          <p
+            className={`${s.description} ${minDescription ? s.minDescription : ""}`}
+          >
+            {description[0]}
+          </p>
+        )}
+        {minDescription &&
+          description?.map((text, index) => (
+            <p
+              className={`${s.description} ${minDescription ? s.minDescription : ""}`}
+              key={index}
+            >
               {text}
             </p>
           ))}
       </div>
+
+      {/* Image Section */}
+      {!leftImg && image && (
+        <figure className={s.image}>
+          <Image
+            src={image}
+            alt="Content Image"
+            width={minDescription ? 1200 : 800}
+            height={minDescription ? 1200 : 800}
+          />
+        </figure>
+      )}
+
+      {/* Additional Descriptions */}
+      {description && !minDescription && description.length > 1 && (
+        <div className={s.addContent}>
+          {description.slice(1).map((text, index) => (
+            <p
+              className={`${s.description} ${minDescription ? s.minDescription : ""}`}
+              key={index}
+            >
+              {text}
+            </p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
-export default ContentSection;
+export default memo(ContentSection);
---
+++ components/innerpage/ImageSection/ImageSection.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import s from "./ImageSection.module.scss";
+import Image from "next/image";
+
+interface ImageSectionProps {
+  image: string;
+  doubleImg?: string[]; // Array of images for double image type
+  type?: string;
+  backgroundImage?: string; // Optional background image
+}
+
+const ImageSection: React.FC<ImageSectionProps> = ({
+  image,
+  type,
+  backgroundImage,
+  doubleImg,
+}) => {
+  return (
+    <div
+      className={`${s.imageSection} ${type === "withBg" ? s.withBg : ""} ${
+        type === "fullImg" || type === "doubleImg" ? s.fullImg : ""
+      }`}
+      style={
+        type === "withBg" && backgroundImage
+          ? { backgroundImage: `url(${backgroundImage})` }
+          : {}
+      }
+    >
+      <figure
+        className={`${s.imageWrap} ${
+          type === "withBg" || type === "fullImg" ? s.fullImgWrap : ""
+        } ${type === "doubleImg" ? s.doubleImgWrap : ""}`}
+      >
+        {type === "withBg" || type === "fullImg" ? (
+          <Image
+            quality={100}
+            src={image}
+            height={1080}
+            width={1920}
+            alt={type || "Image"}
+          />
+        ) : type === "doubleImg" ? (
+          <>
+            {doubleImg?.map((img, index) => (
+              <Image
+                key={index}
+                quality={100}
+                src={img}
+                height={1080}
+                width={1920}
+                alt={`Double image ${index + 1}`}
+              />
+            ))}
+          </>
+        ) : (
+          <Image
+            quality={100}
+            src={image}
+            height={3056}
+            width={2550}
+            alt={type || "Image"}
+          />
+        )}
+      </figure>
+    </div>
+  );
+};
+
+export default ImageSection;
